17daa896ee585a781bcbe7f84d62fbce4f084b83
TMoechel <t.moechel@softcreation.de>
2023-02-05 15:05:13 +0100

    add first output and txt files to save data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userSelection;
+do
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("**************************************");
+    Console.WriteLine("* Welcome to the Loropio Fitness App *");
+    Console.WriteLine("**************************************");
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine("********************");
+    Console.WriteLine("* Select an action *");
+    Console.WriteLine("********************");
+
+    Console.WriteLine("1: Enter new activity");
+    Console.WriteLine("2: View all activities");
+    Console.WriteLine("3: Load specific activity");
+    Console.WriteLine("9: Quit application");
+    Console.Write("Your selection: ");
+
+    userSelection = Console.ReadLine();
+}
+while (userSelection != "9");
+
+
